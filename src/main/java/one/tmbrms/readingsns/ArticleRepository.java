fd733fd8ef285306a63123c4bd5518bd92f9d99b
TAMBARA Masahiko <tambara@jp.ibm.com>
2025-05-12 12:05:12 +0900

    SQLiteでデータを読み込む準備をした

---
+++ src/main/java/one/tmbrms/readingsns/ArticleRepository.java
@@ -0,0 +1,7 @@
+package one.tmbrms.readingsns;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleRepository extends JpaRepository<ArticleDB, Integer> {
+}
+    
