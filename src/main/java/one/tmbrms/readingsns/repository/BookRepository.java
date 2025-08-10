275a94f9fc08e972f0b0b2bf0aaeee7b738f5648
TAMBARA Masahiko <tambara@jp.ibm.com>
2025-08-10 17:44:18 +0900

    bookテーブルのキーの型を間違えていたので修正

--- src/main/java/one/tmbrms/readingsns/repository/BookRepository.java
+++ src/main/java/one/tmbrms/readingsns/repository/BookRepository.java
@@ -3,6 +3,6 @@ package one.tmbrms.readingsns.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import one.tmbrms.readingsns.entity.Book;
 
-public interface BookRepository extends JpaRepository<Book, Integer> {
+public interface BookRepository extends JpaRepository<Book, String> {
 }
     
